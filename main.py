d6cb5b66a6884e818c5272274363ac07c0f8f283
lahcen <lahcenerrich@gmail.com>
2025-06-14 18:45:00 -0400

    add gitignore

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
-api.get('/')
+@api.get('/')
 def index():
     return {"message": "Hello World"}
+
+@api.get('/about')
+def about():
+    return {"data": "about page"}
